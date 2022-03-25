3a012271fde0a76e96a699e7c96b037d9180a120
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-03-25 10:27:12 +0100

    10h27

--- amber-web/src/components/tableObject.js
+++ amber-web/src/components/tableObject.js
@@ -3,22 +3,28 @@ import cv2 from '../assets/cv2.png'
 import cv3 from'../assets/cv3.png'
 
 let objetsList = [
-    { name: "cv1", description: "curriculum vitae", price: "10.50", image: cv1 },
-    { name:"cv2", description: "curriculum vitae", price: "11.75", image: cv2 },
-    { name:"cv3", description: "curriculum vitae", price: "11.25", image: cv3 },
+    { name: "cv1", description: "curriculum vitae1", price: "10.50", image: cv1 },
+    { name:"cv2", description: "curriculum vitae2", price: "11.75", image: cv2 },
+    { name:"cv3", description: "curriculum vitae3", price: "11.25", image: cv3 },
 ]
 
 export default function objetsListfun() {
     return (
-        <ul>
-            {objetsList.map(({ name, description, price, image }) =>
-                <ul>
-                    <li>{name}</li>
-                    <img src={image} />
-                    <li>{price}</li>
-                    <li>{description}</li>
-                </ul>
-            )}
-            </ul>
-    )
+      <ul>
+        {objetsList.map(({ name, description, price, image }) => (
+          <ul>
+            <li>{name}</li>
+            <a href={description}>
+              {/* target="_blank" */}
+              <img src={image} />
+            </a>
+            <li>{price}</li>
+            <li>{description}</li>
+            <br></br>
+            <br></br>
+            <br></br>
+          </ul>
+        ))}
+      </ul>
+    );
 }
