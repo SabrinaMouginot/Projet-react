e0604c12b58afd804f6b3f2763820c8ac1e4d47d
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-03-25 13:30:39 +0100

    13h30

--- amber-web/src/components/tableObject.js
+++ amber-web/src/components/tableObject.js
@@ -4,31 +4,33 @@ import cv3 from'../assets/cv3.png'
 
 
 let objetsList = [
-    { name: "cv1", description: "curriculum vitae1", price: "10.50€", image: cv1 },
-    { name: "cv2", description: "curriculum vitae2", price: "11.75€", image: cv2 },
-    { name: "cv3", description: "curriculum vitae3", price: "11.25€", image: cv3 },
+    { name: " cv1 ", description: " curriculum vitae1 ", price: " 10.50€ ", image: cv1 },
+    { name: " cv2 ", description: " curriculum vitae2 ", price: " 11.75€ ", image: cv2 },
+    { name: " cv3 ", description: " curriculum vitae3 ", price: " 11.25€ ", image: cv3 },
 ]
+
 
 export default function objetsListfun() {
     function handleClick(cequejeveux) {
       alert(cequejeveux);
     }
     return (
-      <ul>
+      <ul class="liste">
         {objetsList.map(({ name, description, price, image }) => (
-          <ul>
-            <li>{name}</li>
-            <img src={image} class="imageCv"/>
-                {/* < href={description} */}
-                <button onClick={() => handleClick(description)}>description</button>
-            
-            <li>{price}</li>
-            <li>{description}</li>
+          <ul >
+            <li> {name} </li>
+            <li> {description} </li>
+            <li> {price} </li>
+            <img src={image} class="imageCv" />
+            {/* < href={description} */}
+            <button onClick={() => handleClick(description)}>
+              description
+            </button>
             <br></br>
             <br></br>
             <br></br>
           </ul>
         ))}
-      </ul>
-    );
+            </ul>
+    )
 }
